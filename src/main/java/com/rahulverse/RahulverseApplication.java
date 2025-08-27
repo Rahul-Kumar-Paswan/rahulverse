3664da5bfc440419a9e4651863bf44f4f6eda046
Rahul-Kumar-Paswan <rahulkumarpaswan941+github@gmail.com>
2025-08-27 18:43:29 +0530

    Docker/docker-compose setup

---
+++ src/main/java/com/rahulverse/RahulverseApplication.java
@@ -0,0 +1,13 @@
+package com.rahulverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulverseApplication.class, args);
+	}
+
+}
